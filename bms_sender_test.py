0a158499300f67986dc50be2dd67e41476f06e8b
ccharan94 <77478060+ccharan94@users.noreply.github.com>
2021-05-30 23:59:22 +0530

    Update bms_sender_test.py

--- bms_sender_test.py
+++ bms_sender_test.py
@@ -2,7 +2,7 @@ import unittest
 import BatteryManagementSystem
 class test_bms_streaming(unittest.TestCase):
     def test_incorrect_stream_format(self):
-        self.assertTrue(BatteryManagementSystem.getBMSStream(stream_length = 100, stream_format = "list") == 'UNSUPPORTED_STREAM_FORMAT")
+        self.assertTrue(BatteryManagementSystem.getBMSStream(stream_length = 100, stream_format = "list") == 'UNSUPPORTED_STREAM_FORMAT')
     def test_bms_stream_in_json_format(self):
         BatteryManagementSystem.getBMSStream(stream_length = 100, stream_format = "json")
     def test_bms_stream_is_json(self):
