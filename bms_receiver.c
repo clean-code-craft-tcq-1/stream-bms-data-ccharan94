e2bcd8b1fef387fe9192448072ce691352a75eef
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-06-21 02:12:32 +0530

    Update bms_receiver.c

--- bms_receiver.c
+++ bms_receiver.c
@@ -13,7 +13,7 @@ char TemperatureValue[Length];
  float AttributeValue1, AttributeValue2=0;
  while (scanf("%s%f,%s%f\n", Attribute1, &AttributeValue1, Attribute2, &AttributeValue2)!=EOF)
  {
-   printf("%[^{]s%0.2f,%s0.2%f\n",Attribute1, AttributeValue1, Attribute2, AttributeValue2);
+   printf("%s%0.2f,%s0.2%f\n",Attribute1, AttributeValue1, Attribute2, AttributeValue2);
    TemperatureValue[line]=AttributeValue1;
    SoCValue[line]= AttributeValue2;
    line++;
