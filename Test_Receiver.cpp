804022f9af0a9674840dc2921531a89724ea9291
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-06-22 01:01:03 +0530

    Update Test_Receiver.cpp

--- Test_Receiver.cpp
+++ Test_Receiver.cpp
@@ -1,4 +1,4 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
 
 #include "test/catch.hpp"
 #include "BMS_AttributeDataSender.h"
@@ -6,12 +6,11 @@
 TEST_CASE("To check if file was successfully opened to read") 
 {
  
-  REQUIRE(BMS_Readfromdatafile()== Success); 
+  REQUIRE(BMS_ReadfromConsole()== Success); 
 }
 
-TEST_CASE("To check data is printed on the console") 
+/*TEST_CASE("To check data is printed on the console") 
 {
-  DataTransmitMode UserRequest=SendData;
   REQUIRE(BMS_WriteToConsole(UserRequest)== Success);
   
-}
+}*/
