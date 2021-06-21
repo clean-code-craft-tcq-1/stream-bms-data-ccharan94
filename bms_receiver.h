008fa2ef4722ff9af1beb2c19726fbb4b6289de7
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-06-21 23:42:28 +0530

    Update bms_receiver.h

--- bms_receiver.h
+++ bms_receiver.h
@@ -8,14 +8,15 @@ typedef enum {
   Success
 } OperationMode;
 
-extern int Max_ArraySize=1024;
+#define Max_ArraySize=1024;
+#define NumberOfAttributes=2;
 extern float Temperature[Max_ArraySize];
 extern float ChargeRate[Max_ArraySize];
 
 extern float MinimumAttributeValueArray[NumberOfAttributes]= {0,0};
 extern float MaximumAttributeValueArray[NumberOfAttributes]= {0,0};
 
-extern OperationMode BMS_Readfromdatafile(line)
+extern OperationMode BMS_ReadfromConsole();
 extern void Calculate_TemperatureMinandMaxRange(int NoOfEnteries)
 extern void Calculate_ChargeRateMinandMaxRange(int NoOfEnteries)
 extern void Calculate_MaxParameterValue(float AttributeValue[int NoOfEnteries], float *MaximumAttributeValue)
