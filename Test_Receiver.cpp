0df1b47bc172c8f4d54fbd5bdb741f7380518d1a
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-06-22 01:25:42 +0530

    Update Test_Receiver.cpp

--- Test_Receiver.cpp
+++ Test_Receiver.cpp
@@ -9,8 +9,8 @@ TEST_CASE("To check if file was successfully opened to read")
   REQUIRE(BMS_ReadfromConsole()== Success); 
 }
 
-/*TEST_CASE("To check data is printed on the console") 
+TEST_CASE("To check data is printed on the console") 
 {
-  REQUIRE(BMS_WriteToConsole(UserRequest)== Success);
+  REQUIRE(ReceiveDatafromSender()== Success);
   
-}*/
+}
