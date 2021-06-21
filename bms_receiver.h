eb7d0fca7eb3a66e221ee14ea6622a6bfdaf345e
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-06-21 23:26:06 +0530

    Update bms_receiver.h

--- bms_receiver.h
+++ bms_receiver.h
@@ -1,1 +1,26 @@
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+typedef enum {
+  Failure,
+  Success
+} OperationMode;
+
+extern int Max_ArraySize=1024;
+extern float Temperature[Max_ArraySize];
+extern float ChargeRate[Max_ArraySize];
+
+extern float MinimumAttributeValueArray[NumberOfAttributes]= {0,0};
+extern float MaximumAttributeValueArray[NumberOfAttributes]= {0,0};
+
+extern OperationMode BMS_Readfromdatafile(line)
+extern void Calculate_TemperatureMinandMaxRange(int NoOfEnteries)
+extern void Calculate_ChargeRateMinandMaxRange(int NoOfEnteries)
+extern void Calculate_MaxParameterValue(float AttributeValue[int NoOfEnteries], float *MaximumAttributeValue)
+extern void Calculate_MinParameterValue(float AttributeValue[int NoOfEnteries], float *MinimumAttributeValue)
+extern void Calculate_TemperatureSimpleMovingAverage(int NoOfEnteries)
+extern void Calculate_ChargeRateSimpleMovingAverage(int NoOfEnteries)
+extern float Calculate_SimpleMovingAverage(float AttributeValue[NoOfEnteries])
+  
