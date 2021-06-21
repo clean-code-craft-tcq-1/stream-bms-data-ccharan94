fa0376f9bde2e0016c7dbfdfdda4bc90c5d3c43b
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-06-22 03:49:43 +0530

    Update bms_receiver.h

--- bms_receiver.h
+++ bms_receiver.h
@@ -11,7 +11,7 @@ typedef enum {
 #define Max_ArraySize 1024
 #define NumberOfAttributes 2
 extern float Temperature[Max_ArraySize];
-extern float ChargeRate[Max_ArraySize];
+extern float SoC[Max_ArraySize];
 extern int NoOfEntery;
 extern float MinimumAttributeValueArray[NumberOfAttributes];
 extern float MaximumAttributeValueArray[NumberOfAttributes];
@@ -19,11 +19,11 @@ extern float MaximumAttributeValueArray[NumberOfAttributes];
 
 extern OperationMode BMS_ReadfromConsole();
 extern void Calculate_TemperatureMinandMaxRange(int NoOfEnteries);
-extern void Calculate_ChargeRateMinandMaxRange(int NoOfEnteries);
+extern void Calculate_SoCMinandMaxRange(int NoOfEnteries);
 extern void Calculate_MaxParameterValue(float AttributeValue[Max_ArraySize], int NoOfEnteries, float *MaximumAttributeValue);
 extern void Calculate_MinParameterValue(float AttributeValue[Max_ArraySize], int NoOfEnteries, float *MinimumAttributeValue);
 extern void Calculate_TemperatureSimpleMovingAverage(int NoOfEnteries);
-extern void Calculate_ChargeRateSimpleMovingAverage(int NoOfEnteries);
+extern void Calculate_SoCSimpleMovingAverage(int NoOfEnteries);
 extern float Calculate_SimpleMovingAverage(float AttributeValue[Max_ArraySize], int NoOfEnteries);
 extern OperationMode ReceiveDatafromSender();
   
